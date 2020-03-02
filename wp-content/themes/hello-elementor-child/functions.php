426350905013784ae8591a09bda1bb406d20f6a3
Bill Jensen <wcjensen@gmail.com>
2020-03-01 23:17:19 -0500

    removing admin bar items

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -30,3 +30,22 @@ function hello_child_enqueue_parent_styles() {
 if (function_exists("register_sidebar")) {
    register_sidebar();
  }
+
+/**
+ * Remove admin bar items
+ * 
+ * @link https://themeisle.com/blog/why-and-how-to-customize-the-wordpress-toolbar/
+ * 
+ * @link https://gist.github.com/nschaeferhoff/5757bd60a27ceb226610a5883867d574
+ * more ideads on custom widgets
+ */
+
+ function remove_toolbar_items($wp_adminbar) {
+   $wp_adminbar->remove_node('comments');
+   $wp_adminbar->remove_node('wp-logo');
+   $wp_adminbar->remove_node('new-content');
+   $wp_adminbar->remove_node('delete-cache');
+   $wp_adminbar->remove_node('stats');
+ }
+ 
+ add_action('admin_bar_menu', 'remove_toolbar_items', 999);
